bca2884f8b66194e292138fb84e6eb328f6f4c86
thedarshanpgowda <sdarshanpgowda2003@gmail.com>
2025-04-19 07:51:43 +0530

    final commit

--- components/Landing1.js
+++ components/Landing1.js
@@ -1,7 +1,16 @@
 'use client'
 import React, { useState } from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Title() {
+    const pathname = usePathname();
+
+    const navItems = [
+        { name: 'Real Time Dashboard', path: '/' },
+        { name: 'Forecast', path: '/forecast' },
+        { name: 'Get Data', path: '/get-data' }
+    ];
 
     return (
         <div className="w-full pt-8 pb-4">
@@ -12,7 +21,22 @@ export default function Title() {
                 <p className="mt-1 text-lg text-gray-500 font-light">
                     Mission Control Dashboard
                 </p>
-                <div className="mt-2 border-b border-gray-200"></div>
+                <div className="mt-6 border-b border-gray-200"></div>
+
+                <div className="flex mt-4 space-x-6">
+                    {navItems.map((item) => (
+                        <Link
+                            key={item.path}
+                            href={item.path}
+                            className={`pb-2 px-1 text-sm font-medium transition-colors duration-200 ${pathname === item.path
+                                    ? 'text-blue-600 border-b-2 border-blue-600'
+                                    : 'text-gray-500 hover:text-gray-900'
+                                }`}
+                        >
+                            {item.name}
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
     );
